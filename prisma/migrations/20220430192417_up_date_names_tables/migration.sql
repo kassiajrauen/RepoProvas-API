d37761b18ce54831017e121ccb68cc498b42db4c
KassiaJRauen <kassia.grj@gmail.com>
2022-04-30 16:44:11 -0300

    update names of tables

---
+++ prisma/migrations/20220430192417_up_date_names_tables/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "teachersDisciplines" DROP CONSTRAINT "teachersDisciplines_disciplineId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "teachersDisciplines" DROP CONSTRAINT "teachersDisciplines_teacherId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tests" DROP CONSTRAINT "tests_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tests" DROP CONSTRAINT "tests_teacherDisciplineId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "teachersDisciplines" ADD CONSTRAINT "teachersDisciplines_disciplineId_fkey" FOREIGN KEY ("disciplineId") REFERENCES "disciplines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teachersDisciplines" ADD CONSTRAINT "teachersDisciplines_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "teachers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tests" ADD CONSTRAINT "tests_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tests" ADD CONSTRAINT "tests_teacherDisciplineId_fkey" FOREIGN KEY ("teacherDisciplineId") REFERENCES "teachersDisciplines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
